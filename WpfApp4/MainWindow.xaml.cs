45b1fa7268b20829262956c0f8b8719411240163
Gigachad1488 <iluhadolmatov@bk.ru>
2023-03-22 09:30:33 +0700

    ЕБЕЙШАЯ ОБНОВА

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -13,7 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
-
+using System.IO;
+using Microsoft.Win32;
 namespace WpfApp4
 {
     /// <summary>
@@ -30,8 +31,14 @@ namespace WpfApp4
                 i += 2;
                 FontSizeComboBox.Items.Add(i);
             }
+            for (double i = 1; i <= 2;)
+            {
+                IntComboBox.Items.Add(i);
+                i += 0.5;
+            }
             var timer = new System.Windows.Threading.DispatcherTimer();
-            timer.Interval = new TimeSpan(1000000);
+            RichTextBox.Document.Blocks.Clear();
+            timer.Interval = new TimeSpan(500000);
             timer.Tick += new EventHandler(timerTick);
             timer.Start();
         }
@@ -49,16 +56,30 @@ namespace WpfApp4
 
         }
 
+        public void SaveSelection()
+        {
+            RichTextBox.Focus();
+        }
+
         private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             Color color = (Color)ColorPicker.SelectedColor;
+            RichTextBox.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(color));
         }
-
         private void CurvButton_Click(object sender, RoutedEventArgs e)
         {
 
-            RichTextBox.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+            RichTextBox.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, RichTextBox.Selection.GetPropertyValue(TextElement.FontStyleProperty).Equals(FontStyles.Italic) ? FontStyles.Normal : FontStyles.Italic);
+        }
 
+        private void FocButton_Click(object sender, RoutedEventArgs e)
+        {
+            RichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, RichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty).Equals(TextDecorations.Underline) ? null : TextDecorations.Underline);
+        }
+
+        private void StrButton_Click(object sender, RoutedEventArgs e)
+        {
+            RichTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, RichTextBox.Selection.GetPropertyValue(TextElement.FontWeightProperty).Equals(FontWeights.Bold) ? FontWeights.Normal : FontWeights.Bold);
         }
 
         private void FontChangeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -68,17 +89,42 @@ namespace WpfApp4
 
         private void FontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            RichTextBox.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, FontSizeComboBox.FontSize);
+            RichTextBox.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, Convert.ToDouble(FontSizeComboBox.SelectedItem));
         }
 
-        private void FocButton_Click(object sender, RoutedEventArgs e)
+        private void SaveFileButton_Click(object sender, RoutedEventArgs e)
         {
-            RichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+            TextRange textrange = new TextRange(RichTextBox.Document.ContentStart, RichTextBox.Document.ContentEnd);
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.Filter = "текстовый документ (*.txt)|*.txt";
+            savefile.Title = "сохранение";
+            if (savefile.ShowDialog() == true)
+            {
+                using (StreamWriter sw = new StreamWriter(savefile.FileName))
+                {
+                    sw.Write(textrange.Text);
+                }
+            }
         }
 
-        private void StrButton_Click(object sender, RoutedEventArgs e)
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
         {
-            RichTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Filter = "текстовый документ (*.txt)|*.txt";
+            openfile.Title = "сохранение";
+            if (openfile.ShowDialog() == true)
+            {
+                using (StreamReader sw = new StreamReader(openfile.FileName))
+                {
+                    string s = sw.ReadToEnd();
+                    RichTextBox.Document.Blocks.Add(new Paragraph(new Run(s)));
+                }
+            }
+        }
+
+        private void IntComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
